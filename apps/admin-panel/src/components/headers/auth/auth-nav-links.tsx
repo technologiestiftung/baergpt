f9c14cab92a9e463594a4ae6ae8d12705c22e814
Zainab Tariq <zainabtariq12@gmail.com>
2025-10-30 15:43:20 +0100

    feat: update header styles for auth layout

--- apps/admin-panel/src/components/headers/auth/auth-nav-links.tsx
+++ apps/admin-panel/src/components/headers/auth/auth-nav-links.tsx
@@ -10,7 +10,7 @@ export function AuthNavLinks() {
 				hidden md:flex gap-x-2 h-9 text-dunkelblau-100 px-3 py-2
 				rounded-3px border border-dunkelblau-100
 				hover:bg-hellblau-60 items-center
-				focus-visible:outline-default text-base leading-6 font-normal
+				focus-visible:outline-default text-lg leading-7 font-normal
 				`}
 			>
 				{Content["header.navigation.help.ariaLabel"]}
--- apps/frontend/src/components/headers/auth/auth-header.tsx
+++ apps/frontend/src/components/headers/auth/auth-header.tsx
@@ -4,10 +4,10 @@ import { AuthNavLinks } from "./auth-nav-links.tsx";
 
 export const AuthHeader: React.FC = () => {
 	return (
-		<header className="h-[50px] md:h-11 flex items-center justify-between z-40 bg-white p-5 lg:px-[50px] border-b border-black">
+		<header className="h-[50px] md:h-[80px] flex items-center justify-between z-40 bg-white p-5 lg:px-[50px] border-b border-black">
 			<a href={"/"}>
 				<img
-					className="h-7"
+					className="h-10"
 					src="/logos/baergpt-logo.svg"
 					alt={Content["header.logo.alt"]}
 				/>
