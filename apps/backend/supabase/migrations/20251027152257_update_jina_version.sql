a0352b5cfcbfabd1273446fafa034b813396ae99
malte-b <malte.barth01@gmail.com>
2025-11-05 17:36:06 +0100

    feat: use new api signature

--- apps/backend/supabase/migrations/20251027152257_update_jina_version.sql
+++ apps/backend/supabase/migrations/20251027152257_update_jina_version.sql
@@ -1,5 +1,7 @@
 ALTER TABLE document_chunks
-ALTER COLUMN chunk_jina_embeddings SET DATA TYPE VECTOR(2048);
+ALTER COLUMN chunk_jina_embeddings
+SET DATA TYPE vector (2048);
 
 ALTER TABLE document_summaries
-ALTER COLUMN summary_jina_embeddings SET DATA TYPE VECTOR(2048);
+ALTER COLUMN summary_jina_embeddings
+SET DATA TYPE vector (2048);
