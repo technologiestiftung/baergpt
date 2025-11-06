c58deca8f8489c566b7c52f242b3209ed7c20b00
malte-b <malte.barth01@gmail.com>
2025-11-06 14:26:41 +0100

    chore: remove migration since we now just truncate the embeddings to have the same size

--- apps/backend/supabase/migrations/20251027152257_update_jina_version.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE document_chunks
-ALTER COLUMN chunk_jina_embedding
-SET DATA TYPE vector (2048);
-
-ALTER TABLE document_summaries
-ALTER COLUMN summary_jina_embedding
-SET DATA TYPE vector (2048);
