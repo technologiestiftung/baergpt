d4505da9dba001ba031761a4635830d1ce5cb25a
Malte Barth <malte.barth@posteo.de>
2025-11-11 16:13:55 +0100

    Feat: Make Backend function compatible (#75)

--- apps/backend/src/monitoring/instrumentation.ts
+++ apps/backend/src/monitoring/instrumentation.ts
@@ -4,18 +4,13 @@ import { LangfuseSpanProcessor, ShouldExportSpan } from "@langfuse/otel";
 import { config } from "../config";
 import { SentryPropagator, SentrySampler } from "@sentry/opentelemetry";
 import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import { supabase } from "../supabase";
 
 export const sentryClient = Sentry.init({
 	dsn: config.sentryDsn || "",
 	environment: config.nodeEnv || "development",
-	integrations: [
-		nodeProfilingIntegration(),
-		Sentry.supabaseIntegration({ supabaseClient: supabase }),
-	],
+	integrations: [Sentry.supabaseIntegration({ supabaseClient: supabase })],
 	tracesSampleRate: 1.0,
-	profileSessionSampleRate: 1.0,
 	enabled: ["production", "staging", "test"].includes(config.nodeEnv),
 	skipOpenTelemetrySetup: true,
 });
