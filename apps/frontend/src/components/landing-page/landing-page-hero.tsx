40cfe72d4db7cd56aecc34316211b83e5637d0a4
aeschi <56318362+aeschi@users.noreply.github.com>
2025-11-24 16:30:19 +0100

    feat: add register buttons to landing page

--- apps/frontend/src/components/landing-page/landing-page-hero.tsx
+++ apps/frontend/src/components/landing-page/landing-page-hero.tsx
@@ -15,9 +15,7 @@ export function LandingPageHero() {
 				<h3 className="z-10 lg:text-2xl lg:leading-8 sm:text-lg sm:leading-7 font-semibold sm:mb-9 mb-4 text-sm leading-5">
 					{Content["landingPage.hero.h3"]}
 				</h3>
-
-				{/* commented out until release */}
-				{/* <a
+				<a
 					href={Content["landingPage.hero.register.link"]}
 					aria-label={Content["landingPage.hero.register.ariaLabel"]}
 					className={`
@@ -28,7 +26,7 @@ export function LandingPageHero() {
                                 `}
 				>
 					{Content["landingPage.hero.register.label"]}
-				</a> */}
+				</a>
 			</div>
 		</div>
 	);
