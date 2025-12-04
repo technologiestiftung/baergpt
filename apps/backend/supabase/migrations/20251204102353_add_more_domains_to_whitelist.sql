0b03c4d671b502b6c52e16b4c199da95ad321f73
malte-b <malte.barth01@gmail.com>
2025-12-04 11:28:10 +0100

    fix: prettier

--- apps/backend/supabase/migrations/20251204102353_add_more_domains_to_whitelist.sql
+++ apps/backend/supabase/migrations/20251204102353_add_more_domains_to_whitelist.sql
@@ -2,10 +2,10 @@ INSERT INTO
 	public.allowed_email_domains (domain)
 VALUES
 	('lfg-b.de'),
-    ('senbjf-goeurope.de'), 
-    ('aufarbeitung-berlin.de'), 
-    ('bebuepol-berlin.de'), 
-    ('anlaufstelle-buergerbeteiligung.de'), 
-    ('itdz-berlin.de'), 
-    ('sfbb.berlin-brandenburg.de'),
-    ('lme.berlin-brandenburg.de'); 
+	('senbjf-goeurope.de'),
+	('aufarbeitung-berlin.de'),
+	('bebuepol-berlin.de'),
+	('anlaufstelle-buergerbeteiligung.de'),
+	('itdz-berlin.de'),
+	('sfbb.berlin-brandenburg.de'),
+	('lme.berlin-brandenburg.de');
