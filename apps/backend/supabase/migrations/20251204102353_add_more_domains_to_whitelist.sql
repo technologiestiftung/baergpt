7988d4c2f8c93bd1b142d8c98edc39afb635891b
Malte Barth <malte.barth@posteo.de>
2025-12-04 11:52:45 +0100

    Chore: Add more whitelisted domains (#138)

---
+++ apps/backend/supabase/migrations/20251204102353_add_more_domains_to_whitelist.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+	public.allowed_email_domains (domain)
+VALUES
+	('lfg-b.de'),
+	('senbjf-goeurope.de'),
+	('aufarbeitung-berlin.de'),
+	('bebuepol-berlin.de'),
+	('anlaufstelle-buergerbeteiligung.de'),
+	('itdz-berlin.de'),
+	('sfbb.berlin-brandenburg.de'),
+	('lme.berlin-brandenburg.de');
