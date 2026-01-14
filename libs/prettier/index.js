4cbdd6e2bdc6272e7ef01ad0d9c97a0fc95fae84
malte-b <malte.barth01@gmail.com>
2026-01-14 10:26:17 +0100

    fix: change glob pattern

--- libs/prettier/index.js
+++ libs/prettier/index.js
@@ -13,7 +13,7 @@ export default {
 	expressionWidth: 100,
 	overrides: [ // use default of sql plugin: https://github.com/sql-formatter-org/sql-formatter/blob/master/docs/useTabs.md
 		{
-			files: "*.sql",
+			files: "**/*.sql",
 			options: {
 				useTabs: false, // use spaces instead of tabs for SQL, as it would otherwise would get overwritten by our default prettier config for TS
 				tabWidth: 4,
