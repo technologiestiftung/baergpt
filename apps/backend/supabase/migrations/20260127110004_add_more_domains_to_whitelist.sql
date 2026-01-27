bfc1edf71c2df6c178732799910b585ec9f33b73
Rapha <raphael.arce@ts.berlin>
2026-01-27 12:02:18 +0100

    feat: add additional domains to the whitelist for allowed email addresses

---
+++ apps/backend/supabase/migrations/20260127110004_add_more_domains_to_whitelist.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+    public.allowed_email_domains (domain)
+VALUES
+    ('gdw-berlin.de'),
+    ('bruecke-museum.de'),
+    ('deutschestheater.de'),
+    ('konzerthaus.de'),
+    ('gorki.de'),
+    ('parkaue.de'),
+    ('volksbuehne-berlin.de'),
+    ('krematorium-berlin.de'),
+    ('lbv.brandenburg.de');
