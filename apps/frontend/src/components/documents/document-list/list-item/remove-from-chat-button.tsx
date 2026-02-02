93bfb56b64922e9c71658ba4aecdd53f2b8cb090
aeschi <56318362+aeschi@users.noreply.github.com>
2026-02-02 17:19:34 +0100

    fix: button text color

--- apps/frontend/src/components/documents/document-list/list-item/remove-from-chat-button.tsx
+++ apps/frontend/src/components/documents/document-list/list-item/remove-from-chat-button.tsx
@@ -10,7 +10,7 @@ export const RemoveFromChatButton: React.FC<RemoveFromChatButtonProps> = ({
 }) => {
 	return (
 		<button
-			className="flex rounded-3px w-fit items-center px-2 gap-1.5 text-dunkelblau-80 bg-hellblau-100 text-sm leading-5 font-normal hover:bg-mittelblau-70 focus-visible:outline-default h-8"
+			className="flex rounded-3px w-fit items-center px-2 gap-1.5 text-dunkelblau-100 bg-hellblau-100 text-sm leading-5 font-normal hover:bg-mittelblau-70 focus-visible:outline-default h-8"
 			type="button"
 			onClick={handleRemoveFromChat}
 		>
