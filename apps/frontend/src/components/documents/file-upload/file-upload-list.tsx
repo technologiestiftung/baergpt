ccf3312faec80ea50dd92c485c6f8f960c524200
Zainab Tariq <zainabtariq12@gmail.com>
2026-02-04 16:17:33 +0100

    fix: update warning message color for max parallel uploads in file upload list

--- apps/frontend/src/components/documents/file-upload/file-upload-list.tsx
+++ apps/frontend/src/components/documents/file-upload/file-upload-list.tsx
@@ -39,7 +39,7 @@ export function FileUploadList() {
 			{isParallelUploadLimitWarningVisible &&
 				hasExceededParallelUploadLimit &&
 				!isMaxParallelUploadWarningVisible && (
-					<div className="border-y-[0.5px] border-dunkelblau-80 w-full p-2.5 bg-dunkelblau-100 text-sm leading-5 font-semibold text-warning-85 z-10 sticky top-0">
+					<div className="border-y-[0.5px] border-dunkelblau-80 w-full p-2.5 bg-dunkelblau-100 text-sm leading-5 font-semibold text-warning-100 z-10 sticky top-0">
 						{`${Content["fileUpload.maxFilesWarning.p1"]} ${import.meta.env.VITE_MAX_PARALLEL_FILE_UPLOADS} ${Content["fileUpload.maxFilesWarning.p2"]}`}
 					</div>
 				)}
