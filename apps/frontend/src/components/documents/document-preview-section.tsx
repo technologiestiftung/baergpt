c76bc4312f17efcf31d35d6e55f293191c414543
aeschi <56318362+aeschi@users.noreply.github.com>
2026-02-11 11:35:49 +0100

    feat: remove zoom

--- apps/frontend/src/components/documents/document-preview-section.tsx
+++ apps/frontend/src/components/documents/document-preview-section.tsx
@@ -96,7 +96,7 @@ export const DocumentPreviewSection: React.FC = () => {
 						<div className="w-full h-full overflow-hidden relative">
 							<iframe
 								key={selectedPreviewDocument.id}
-								src={`${selectedPreviewDocumentPreviewUrl}#toolbar=0&view=fitH&zoom=150`}
+								src={`${selectedPreviewDocumentPreviewUrl}#toolbar=0&view=fitH`}
 								className="absolute -inset-1.5 w-[calc(100%+12px)] h-[calc(100%+12px)] shadow-md"
 								title={Content["documentsPreviewSection.title"]}
 							/>
