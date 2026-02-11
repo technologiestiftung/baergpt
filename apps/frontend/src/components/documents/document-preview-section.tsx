6fb136ca5d1ff24987a0de20e9ca6257b46448b0
aeschi <56318362+aeschi@users.noreply.github.com>
2026-02-11 11:18:03 +0100

    feat: update styling pdf preview

--- apps/frontend/src/components/documents/document-preview-section.tsx
+++ apps/frontend/src/components/documents/document-preview-section.tsx
@@ -30,7 +30,7 @@ export const DocumentPreviewSection: React.FC = () => {
 	return (
 		<section className="absolute h-full inset-0 z-30 flex flex-col bg-hellblau-30">
 			<div className="flex flex-col gap-4 md:gap-5 px-5 md:px-[60px] md:pt-6 md:pb-2 pb-1 pt-2.5">
-				<div className="flex w-full justify-between gap-1 px-0 md:px-2.5">
+				<div className="flex w-full justify-between gap-1">
 					<h2 className="flex text-base leading-6 md:text-xl md:leading-7 font-semibold break-all text-dunkelblau-100">
 						{selectedPreviewDocument?.file_name}
 					</h2>
@@ -57,7 +57,7 @@ export const DocumentPreviewSection: React.FC = () => {
 								height={20}
 								alt={Content["downloadIcon.imgAlt"]}
 							/>
-							<span className="text-sm leading-5 font-normal">
+							<span className="text-sm leading-5 font-normal text-dunkelblau-100">
 								{Content["documentsPreviewSection.downloadLink.label"]}
 							</span>
 						</a>
@@ -77,7 +77,7 @@ export const DocumentPreviewSection: React.FC = () => {
 					)}
 				</div>
 			</div>
-			<div className="h-full w-full px-5 md:px-24 pt-7 md:pt-8 bg-hellblau-30 flex items-center justify-center flex-col">
+			<div className="h-full w-full px-5 md:px-[60px] bg-hellblau-30 flex items-center justify-center flex-col">
 				{!hasSupportedPreview && (
 					<div className="flex items-center justify-center h-full text-center max-w-xl">
 						<p className="text-lg text-dunkelblau-80">
@@ -89,17 +89,17 @@ export const DocumentPreviewSection: React.FC = () => {
 					<>
 						<p
 							className={`pb-5 text-sm leading-5 font-normal text-dunkelblau-80 
-								${isDocxFormat(selectedPreviewDocument.file_name ?? "") ? "" : "hidden"}`}
+						${isDocxFormat(selectedPreviewDocument.file_name ?? "") ? "" : "hidden"}`}
 						>
 							{Content["documentsPreviewSection.disclaimer.docx"]}
 						</p>
-						<iframe
-							key={selectedPreviewDocument.id}
-							src={`${selectedPreviewDocumentPreviewUrl}#toolbar=0&view=fitH`}
-							width="100%"
-							height="100%"
-							className="bg-white shadow-[0px_0px_12px_0px_rgba(3,8,18,0.25)]"
-						/>
+						<div className="w-full h-full overflow-hidden relative">
+							<iframe
+								key={selectedPreviewDocument.id}
+								src={`${selectedPreviewDocumentPreviewUrl}#toolbar=0&view=fitH&zoom=150`}
+								className="absolute -inset-1.5 w-[calc(100%+12px)] h-[calc(100%+12px)] shadow-md"
+							/>
+						</div>
 					</>
 				)}
 				{!selectedPreviewDocumentPreviewUrl && hasSupportedPreview && (
