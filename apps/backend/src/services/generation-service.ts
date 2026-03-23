5ea9736a9bcbbbd75ff28cbc2dac011030264db8
Malte Barth <malte.barth01@gmail.com>
2026-03-23 10:25:11 +0100

    feat: add web search prompt if web search is active (#248)

--- apps/backend/src/services/generation-service.ts
+++ apps/backend/src/services/generation-service.ts
@@ -776,6 +776,7 @@ export class GenerationService {
 	async createPrompt(
 		previousMessages: ModelMessage[],
 		isAddressedFormal: boolean,
+		activeTools: ActiveTools[],
 	): Promise<{
 		messages: ModelMessage[];
 		promptClient: TextPromptClient;
@@ -787,16 +788,37 @@ export class GenerationService {
 		});
 
 		const addressForm = isAddressedFormal ? "Sieze" : "Duze";
-		// Always use free-chat prompt
 		let freeChatPromptClient: TextPromptClient;
-		try {
-			freeChatPromptClient = await langfuse.prompt.get(
-				"free-chat",
-				{ label: config.nodeEnv === "test" ? "development" : config.nodeEnv }, // Fallback to development prompt version during tests
-			);
-		} catch (error) {
-			captureError(error);
-			throw error;
+
+		const effectiveActiveTools = [...activeTools];
+		// TODO: Remove the check for feature flag once frontend functionality to add web search tool in chat is implemented
+		if (
+			config.featureFlagWebSearchAllowed &&
+			!effectiveActiveTools.includes("webSearchTool")
+		) {
+			effectiveActiveTools.push("webSearchTool");
+		}
+
+		if (effectiveActiveTools.includes("webSearchTool")) {
+			try {
+				freeChatPromptClient = await langfuse.prompt.get(
+					"free-chat-with-web-search-enabled",
+					{ label: config.nodeEnv === "test" ? "development" : config.nodeEnv },
+				);
+			} catch (error) {
+				captureError(error);
+				throw error;
+			}
+		} else {
+			try {
+				freeChatPromptClient = await langfuse.prompt.get(
+					"free-chat",
+					{ label: config.nodeEnv === "test" ? "development" : config.nodeEnv }, // Fallback to development prompt version during tests
+				);
+			} catch (error) {
+				captureError(error);
+				throw error;
+			}
 		}
 		const compiledFreeChatPrompt = freeChatPromptClient.compile({
 			currentDate: currentDate,
