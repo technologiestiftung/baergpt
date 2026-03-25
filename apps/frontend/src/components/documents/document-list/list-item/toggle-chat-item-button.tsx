d52c765fa63cfa85cd55aa7007a1b8b756eae211
Rapha <raphael.arce@ts.berlin>
2026-03-25 13:23:08 +0100

    fix: visual glitch on add to chat button

--- apps/frontend/src/components/documents/document-list/list-item/toggle-chat-item-button.tsx
+++ apps/frontend/src/components/documents/document-list/list-item/toggle-chat-item-button.tsx
@@ -17,11 +17,11 @@ export const ToggleChatItemButton: React.FC<ToggleChatItemButtonProps> = ({
 	return (
 		<button
 			className={`hidden md:flex
-			rounded-3px items-center px-2 gap-1.5 text-sm leading-5 font-normal focus-visible:outline-2px h-8 w-0
+			rounded-3px items-center px-2 gap-1.5 text-sm leading-5 font-normal focus-visible:outline-2px h-8 w-fit
 			${
 				isSelectedForChat
-					? "text-dunkelblau-100 bg-hellblau-100 hover:bg-mittelblau-70 w-fit"
-					: "bg-dunkelblau-100 text-hellblau-30 hover:bg-dunkelblau-90 group-hover:opacity-100 group-hover:w-fit focus-within:opacity-100 focus-within:w-fit opacity-0"
+					? "text-dunkelblau-100 bg-hellblau-100 hover:bg-mittelblau-70"
+					: "bg-dunkelblau-100 text-hellblau-30 hover:bg-dunkelblau-90 group-hover:opacity-100  focus-within:opacity-100 opacity-0"
 			}`}
 			type="button"
 			onClick={handleToggleChatItem}
