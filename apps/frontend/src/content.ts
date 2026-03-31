31f670742ede18517d4b71de653ef7ed2188ea10
aeschi <56318362+aeschi@users.noreply.github.com>
2026-03-31 16:43:09 +0200

    feat: text changes data privacy statement (#271)

--- apps/frontend/src/content.ts
+++ apps/frontend/src/content.ts
@@ -82,7 +82,7 @@ export const Content = {
 
 	/* -------------------- Privacy Policy Page -------------------- */
 	"privacyPolicyPage.h1": "Datenschutzerklärung",
-	"privacyPolicyPage.date": "Stand Oktober 2025",
+	"privacyPolicyPage.date": "Stand März 2026",
 	"privacyPolicyPage.tableOfContents.title": "Inhaltsverzeichnis",
 	//Section 1
 	"privacyPolicyPage.section1.title": "Einleitung und Anwendungsbereich",
@@ -181,7 +181,7 @@ export const Content = {
 		"Löschung nach Abschluss der Auswertung",
 	"privacyPolicyPage.section6.li5.title": "Backups: ",
 	"privacyPolicyPage.section6.li5.description":
-		"Löschung nach festem Rotationsplan (derzeit sieben Tage)",
+		"Löschung nach festem Rotationsplan (derzeit 30 Tage)",
 	//Section 7
 	"privacyPolicyPage.section7.title": "Empfänger und Weitergabe von Daten",
 	"privacyPolicyPage.section7.p1":
