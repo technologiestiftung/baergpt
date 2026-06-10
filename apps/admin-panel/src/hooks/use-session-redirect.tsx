df2354f87b011e557e5169ad239bba172cd32742
aeschi <56318362+aeschi@users.noreply.github.com>
2026-06-10 16:42:51 +0200

    fix: block deactivated users from admin dashboard login

--- apps/admin-panel/src/hooks/use-session-redirect.tsx
+++ apps/admin-panel/src/hooks/use-session-redirect.tsx
@@ -2,9 +2,12 @@ import { useEffect } from "react";
 import { useAuthStore } from "../store/use-auth-store.ts";
 import { useLocation, useNavigate } from "react-router-dom";
 import type { Session } from "@supabase/supabase-js";
+import { useAuthErrorStore } from "../store/use-auth-error-store.ts";
+import { useIsActiveStore } from "../store/use-is-active-store.ts";
 
 export function useSessionRedirect() {
 	const session = useAuthStore((state) => state.session);
+	const isActive = useIsActiveStore((state) => state.isActive);
 
 	const location = useLocation();
 	const navigate = useNavigate();
@@ -14,22 +17,21 @@ export function useSessionRedirect() {
 			session,
 			pathname: location.pathname,
 			navigate,
-		});
-
-		if (!session) {
-			return;
-		}
-	}, [session]);
+			isActive,
+		}).catch(console.error);
+	}, [session, isActive, location.pathname, navigate]);
 }
 
-function redirectBasedOnSession({
+async function redirectBasedOnSession({
 	session,
 	pathname,
 	navigate,
+	isActive,
 }: {
 	session: Session | null | undefined;
 	pathname: string;
 	navigate: (path: string) => void;
+	isActive: boolean | null;
 }) {
 	/**
 	 * On first load the session is undefined and
@@ -41,6 +43,18 @@ function redirectBasedOnSession({
 
 	if (!session) {
 		handleUnauthorized(pathname, navigate);
+		return;
+	}
+
+	if (isActive === null) {
+		return;
+	}
+
+	if (isActive === false) {
+		await useAuthStore.getState().logout();
+		useAuthErrorStore
+			.getState()
+			.handleError(new Error("User account has been deactivated."));
 		return;
 	}
 
