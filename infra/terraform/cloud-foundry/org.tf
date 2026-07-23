9ef239d930b9302b1f10d9fc70929afd6ad32ae5
Niels Poensgen <niels.poensgen@gmail.com>
2026-07-23 15:49:12 +0200

    Add STACKIT Cloud Foundry Terraform module

---
+++ infra/terraform/cloud-foundry/org.tf
@@ -0,0 +1,19 @@
+resource "stackit_scf_organization" "org" {
+  project_id = var.project_id
+  name       = var.org_name
+  region     = var.region
+}
+
+# Machine user the CF provider authenticates as.
+# Auto-deleted when the org is removed.
+resource "stackit_scf_organization_manager" "manager" {
+  project_id = var.project_id
+  org_id     = stackit_scf_organization.org.org_id
+  region     = var.region
+}
+
+data "stackit_scf_platform" "platform" {
+  project_id  = var.project_id
+  platform_id = stackit_scf_organization.org.platform_id
+  region      = var.region
+}
