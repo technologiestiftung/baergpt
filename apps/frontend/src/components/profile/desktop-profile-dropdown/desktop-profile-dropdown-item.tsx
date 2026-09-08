f29917d216fe598f3620d4edd7e94cfcc7dfa1f0
Zainab Tariq <zainabtariq12@gmail.com>
2026-09-08 15:30:32 +0200

    feat: update app navigation (#490)
    * feat: update profile dropdown and sidebar navigation, remove app header
    
    * feat: update profile dropdown and mobile drawer navigation styles

--- apps/frontend/src/components/profile/desktop-profile-dropdown/desktop-profile-dropdown-item.tsx
+++ apps/frontend/src/components/profile/desktop-profile-dropdown/desktop-profile-dropdown-item.tsx
@@ -5,28 +5,47 @@ interface DesktopProfileDropdownItemProps {
 	icon: string;
 	label: string;
 	href?: string;
+	openInNewTab?: boolean;
 	onClick?: () => void;
 	ariaLabel?: string;
 }
 
 export const DesktopProfileDropdownItem: React.FC<
 	DesktopProfileDropdownItemProps
-> = ({ type = "link", icon, label, href, onClick, ariaLabel }) => {
+> = ({
+	type = "link",
+	icon,
+	label,
+	href,
+	openInNewTab,
+	onClick,
+	ariaLabel,
+}) => {
 	const classes =
-		"flex rounded-[3px] items-center px-1 py-0.5 gap-2 w-full bg-transparent text-sm leading-5 hover:bg-hellblau-60 focus-visible:bg-hellblau-60 focus-visible:outline-default cursor-pointer";
+		"flex rounded-[3px] items-center justify-center pl-1.5 pr-3 py-1 w-full bg-transparent text-sm leading-5 hover:bg-hellblau-60 focus-visible:bg-hellblau-60 focus-visible:outline-default cursor-pointer";
 
 	if (type === "button") {
 		return (
 			<button className={classes} onClick={onClick} aria-label={ariaLabel}>
-				<img src={icon} alt="" />
+				<div className="p-1">
+					<img src={icon} alt="" className="size-5" />
+				</div>
 				<span>{label}</span>
 			</button>
 		);
 	}
 
 	return (
-		<a href={href} className={classes}>
-			<img src={icon} alt="" />
+		<a
+			href={href}
+			className={classes}
+			aria-label={ariaLabel}
+			target={openInNewTab ? "_blank" : undefined}
+			rel={openInNewTab ? "noopener noreferrer" : undefined}
+		>
+			<div className="p-1">
+				<img src={icon} alt="" className="size-5" />
+			</div>
 			<span>{label}</span>
 		</a>
 	);
